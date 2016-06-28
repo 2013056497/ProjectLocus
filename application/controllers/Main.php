426fcd39df2f3c3046ca50dbb3d3406b01502910
2013056497 <justincastilloust@gmail.com>
2016-06-28 16:40:43 +0800

    1st commit

---
+++ application/controllers/Main.php
@@ -0,0 +1,52 @@
+<?php
+class Main extends CI_Controller {
+
+	/**
+	 * __construct function.
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		
+		parent::__construct();
+		$this->load->library(array('session'));
+		$this->load->helper(array('url'));
+		$this->load->helper(array('form'));
+	}
+
+	public function index(){
+		$this->login();
+	}
+
+	public function login(){
+		$this->load->view('templates/ext_header');
+		$this->load->view('pages/login');
+		$this->load->view('templates/ext_footer');
+	}
+
+	public function dashboard(){
+		$this->load->view('templates/header');
+		$this->load->view('templates/lsidebar');
+		$this->load->view('templates/rsidebar');
+		$this->load->view('pages/dashboard');
+		$this->load->view('templates/footer');
+	}
+
+	public function proj_view(){
+		$this->load->view('templates/header');
+		$this->load->view('templates/lsidebar');
+		$this->load->view('templates/rsidebar');
+		$this->load->view('pages/proj_view');
+		$this->load->view('templates/footer');
+	}
+
+	public function proj_add(){
+		$this->load->view('templates/header');
+		$this->load->view('templates/lsidebar');
+		$this->load->view('templates/rsidebar');
+		$this->load->view('pages/proj_add');
+		$this->load->view('templates/footer');
+	}
+}
+?>
---
+++ application/views/templates/lsidebar.php
@@ -0,0 +1,190 @@
+<!--Left sidebar start-->
+<aside>
+    <div id="sidebar"  class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+            <li>
+                <a class="active" href="<?= site_url('main/dashboard')?>">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-sitemap"></i>
+                    <span>Projects</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="<?= site_url('main/proj_view')?>">View Projects</a></li>
+                    <li><a  href="<?= site_url('main/proj_add')?>">Add Project</a></li>
+                </ul>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-book"></i>
+                    <span>UI Elements</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="general.php">General</a></li>
+                    <li><a  href="buttons.php">Buttons</a></li>
+                    <li><a  href="modal.php">Modal</a></li>
+                    <li><a  href="toastr.php">Toastr Notifications</a></li>
+                    <li><a  href="widget.php">Widget</a></li>
+                    <li><a  href="slider.php">Slider</a></li>
+                    <li><a  href="nestable.php">Nestable</a></li>
+                    <li><a  href="font_awesome.php">Font Awesome</a></li>
+                </ul>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-cogs"></i>
+                    <span>Components</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="grids.php">Grids</a></li>
+                    <li><a  href="calendar.php">Calendar</a></li>
+                    <li><a  href="gallery.php">Gallery</a></li>
+                    <li><a  href="todo_list.php">Todo List</a></li>
+                    <li><a  href="draggable_portlet.php">Draggable Portlet</a></li>
+                    <li><a  href="tree.php">Tree View</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-tasks"></i>
+                    <span>Form Stuff</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="form_component.php">Form Components</a></li>
+                    <li><a  href="advanced_form_components.php">Advanced Components</a></li>
+                    <li><a  href="form_wizard.php">Form Wizard</a></li>
+                    <li><a  href="form_validation.php">Form Validation</a></li>
+                    <li><a  href="dropzone.php">Dropzone File Upload</a></li>
+                    <li><a  href="inline_editor.php">Inline Editor</a></li>
+                    <li><a  href="image_cropping.php">Image Cropping</a></li>
+                    <li><a  href="file_upload.php">Multiple File Upload</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-th"></i>
+                    <span>Data Tables</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="basic_table.php">Basic Table</a></li>
+                    <li><a  href="responsive_table.php">Responsive Table</a></li>
+                    <li><a  href="dynamic_table.php">Dynamic Table</a></li>
+                    <li><a  href="editable_table.php">Editable Table</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class=" fa fa-envelope"></i>
+                    <span>Mail</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="inbox.php">Inbox</a></li>
+                    <li><a  href="inbox_details.php">Inbox Details</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class=" fa fa-bar-chart-o"></i>
+                    <span>Charts</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="morris.php">Morris</a></li>
+                    <li><a  href="chartjs.php">Chartjs</a></li>
+                    <li><a  href="flot_chart.php">Flot Charts</a></li>
+                    <li><a  href="xchart.php">xChart</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-shopping-cart"></i>
+                    <span>Shop</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="product_list.php">List View</a></li>
+                    <li><a  href="product_details.php">Details View</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="google_maps.php" >
+                    <i class="fa fa-map-marker"></i>
+                    <span>Google Maps </span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-comments-o"></i>
+                    <span>Chat Room</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="lobby.php">Lobby</a></li>
+                    <li><a  href="chat_room.php"> Chat Room</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-glass"></i>
+                    <span>Extra</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="blank.php">Blank Page</a></li>
+                    <li><a  href="sidebar_closed.php">Sidebar Closed</a></li>
+                    <li><a  href="people_directory.php">People Directory</a></li>
+                    <li><a  href="coming_soon.php">Coming Soon</a></li>
+                    <li><a  href="lock_screen.php">Lock Screen</a></li>
+                    <li><a  href="profile.php">Profile</a></li>
+                    <li><a  href="invoice.php">Invoice</a></li>
+                    <li><a  href="project_list.php">Project List</a></li>
+                    <li><a  href="project_details.php">Project Details</a></li>
+                    <li><a  href="search_result.php">Search Result</a></li>
+                    <li><a  href="pricing_table.php">Pricing Table</a></li>
+                    <li><a  href="faq.php">FAQ</a></li>
+                    <li><a  href="fb_wall.php">FB Wall</a></li>
+                    <li><a  href="404.php">404 Error</a></li>
+                    <li><a  href="500.php">500 Error</a></li>
+                </ul>
+            </li>
+            <li>
+                <a  href="login.php">
+                    <i class="fa fa-user"></i>
+                    <span>Login Page</span>
+                </a>
+            </li>
+
+            <!--multi level menu start-->
+            <li class="sub-menu">
+                <a href="javascript:;" >
+                    <i class="fa fa-sitemap"></i>
+                    <span>Multi level Menu</span>
+                </a>
+                <ul class="sub">
+                    <li><a  href="javascript:;">Menu Item 1</a></li>
+                    <li class="sub-menu">
+                        <a  href="boxed_page.php">Menu Item 2</a>
+                        <ul class="sub">
+                            <li><a  href="javascript:;">Menu Item 2.1</a></li>
+                            <li class="sub-menu">
+                                <a  href="javascript:;">Menu Item 3</a>
+                                <ul class="sub">
+                                    <li><a  href="javascript:;">Menu Item 3.1</a></li>
+                                    <li><a  href="javascript:;">Menu Item 3.2</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!--multi level menu end-->
+
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+<!--Left sidebar end-->
