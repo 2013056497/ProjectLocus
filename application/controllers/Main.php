49ce17f59823e16a222a4d49a1e127f1e958a112
Joshua T. Manaol <joshua.manaol@gmail.com>
2016-06-29 15:57:13 +0800

    u

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -34,11 +34,9 @@ class Main extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
-<<<<<<< HEAD
+
 	public function manage_projects(){
-=======
-	public function projects(){
->>>>>>> 12c43593d3b69a794558e4ebadd99caf2cc6489d
+
 		$this->load->view('templates/header');
 		$this->load->view('templates/lsidebar');
 		$this->load->view('templates/rsidebar');
